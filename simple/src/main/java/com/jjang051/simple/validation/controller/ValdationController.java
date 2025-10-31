8b958ea0380257aa80f37f637aa55dedc86fa2cb
jjang051 <jjang051jhta@gmail.com>
2025-10-31 13:38:21 +0900

    simple

--- simple/src/main/java/com/jjang051/simple/validation/controller/ValdationController.java
+++ simple/src/main/java/com/jjang051/simple/validation/controller/ValdationController.java
@@ -1,8 +1,10 @@
 package com.jjang051.simple.validation.controller;
 
 import com.jjang051.simple.validation.dto.ValidDto;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +13,37 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class ValdationController {
     @GetMapping("/valid01")
-    public String valid01(@ModelAttribute(name = "validDto") ValidDto validDto) {
+    public String valid01(@ModelAttribute(name = "validDto") ValidDto validDto,
+                          Model model) {
+        model.addAttribute("validDto", new ValidDto());
         //log.info("validDto = {}", validDto);
         return "valid/valid01";
     }
     @PostMapping("/valid01")
     //@ResponseBody
-    public String valid01Process(@ModelAttribute(name = "validDto") ValidDto validDto) {
+    public String valid01Process(@Valid
+                            @ModelAttribute(name = "validDto") ValidDto validDto,
+                                 BindingResult bindingResult) {
         log.info("validDto = {}", validDto);
-        if(validDto.getUserEmail().isBlank()){
+        boolean passwordConfirm = false;
+        boolean emailConfirm = false;
+        boolean isAgree = false;
+
+        if(passwordConfirm==false){
+            bindingResult.reject("passwordMismatch","비밀번호 확인해 주세요");
+        }
+        if(emailConfirm==false){
+            bindingResult.reject("emailMismatch","이메일이 일치하지 않습니다.");
+        }
+        if(isAgree==false){
+            //global error
+            bindingResult.reject("isAgree","이용약관에 체크해주세요.");
+        }
+        if(validDto.getUserEmail().equals("jjang051@hanmail.net")) {
+            bindingResult.rejectValue("userEmail","duplicate","사용중인 이메일입니다.");
+
+        }
+        if(bindingResult.hasErrors()){
             return "valid/valid01";
         }
         return "/";
